e5a8667cd41f76dc858888309a6874559b9c137c
jeffreyrosenbluth <jeffrey.rosenbluth@gmail.com>
2024-01-02 23:50:54 -0700

    dots

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,101 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+use image::{imageops, RgbaImage};
+use serde::{Deserialize, Serialize};
+use std::sync::Mutex;
+use wassily::prelude::*;
+
+const W: f32 = 1024.0;
+const CELL_SIZE: u32 = 20;
+
+// Shared state for the tauri app.
+struct State {
+    base_image: Mutex<RgbaImage>,
+}
+
+// Data to send to the js side for rendering the image.
+#[derive(Serialize)]
+struct Picture {
+    width: u32,
+    height: u32,
+    data: Vec<u8>,
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .manage(State {
+            base_image: Mutex::new(RgbaImage::new(0, 0)),
+        })
+        .invoke_handler(tauri::generate_handler![get_image, gen_image, save_image])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+// Open the image and store it in the global state.
+// Scale it to the canvas size before sending it to the js side.
+#[tauri::command]
+fn get_image(path: &str, state: tauri::State<State>) -> Result<Picture, String> {
+    let img = image::open(path)
+        .map_err(|err| format!("The file at {} could not be opened: {}", path, err))?;
+    let mut state_base_image = state.base_image.lock().expect("Could not lock state mutex");
+    *state_base_image = img.to_rgba8();
+    let scale = W / img.width() as f32;
+    let nwidth = (img.width() as f32 * scale) as u32;
+    let nhight = (img.height() as f32 * scale) as u32;
+    let new_img = imageops::resize(&img, nwidth, nhight, imageops::FilterType::Lanczos3);
+    Ok(Picture {
+        width: nwidth,
+        height: nhight,
+        data: new_img.into_vec(),
+    })
+}
+
+#[tauri::command]
+fn gen_image(state: tauri::State<State>) -> Picture {
+    let img = generate(state);
+    let scale = W / img.width() as f32;
+    let nwidth = (img.width() as f32 * scale) as u32;
+    let nhight = (img.height() as f32 * scale) as u32;
+    let new_img = imageops::resize(&img, nwidth, nhight, imageops::FilterType::Lanczos3);
+    Picture {
+        width: nwidth,
+        height: nhight,
+        data: new_img.into_vec(),
+    }
+}
+
+fn generate(state: tauri::State<State>) -> RgbaImage {
+    let in_img = state
+        .base_image
+        .lock()
+        .expect("Could not lock state mutex")
+        .clone();
+    let width = CELL_SIZE * in_img.width();
+    let height = CELL_SIZE * in_img.height();
+    let mut canvas = Canvas::new(width, height);
+    canvas.fill(*WHITE);
+    for x in 0..in_img.width() {
+        for y in 0..in_img.height() {
+            let pixel = in_img.get_pixel(x, y);
+            let color = (Color::from_rgba8(pixel[0], pixel[1], pixel[2], 255)).grayscale();
+            let radius = 1.0 - color.red();
+            Shape::new()
+                .circle(
+                    pt(x * CELL_SIZE + CELL_SIZE / 2, y * CELL_SIZE + CELL_SIZE / 2),
+                    radius * CELL_SIZE as f32 * 0.5,
+                )
+                .fill_color(*SADDLEBROWN)
+                .no_stroke()
+                .draw(&mut canvas);
+        }
+    }
+    let out_img = canvas.into();
+    out_img
+}
+
+#[tauri::command]
+fn save_image(path: &str, state: tauri::State<State>) {
+    let gen = generate(state);
+    let _ = gen.save(path);
 }
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,130 @@
 import { invoke } from "@tauri-apps/api/tauri";
+import { dialog } from "@tauri-apps/api";
+import GUI from "lil-gui";
 
-let greetInputEl: HTMLInputElement | null;
-let greetMsgEl: HTMLElement | null;
+interface Picture {
+  width: number;
+  height: number;
+  data: Uint8Array;
+}
 
-async function greet() {
-  if (greetMsgEl && greetInputEl) {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    greetMsgEl.textContent = await invoke("greet", {
-      name: greetInputEl.value,
-    });
+const W = 1024;
+const gui = new GUI();
+
+// Open an image and save it to the global state.
+// Then display it in the main window.
+async function chooseImage() {
+  try {
+    // Query the user for the filepath.
+    const file = (await dialog.open({
+      multiple: false,
+      directory: false,
+      filters: [
+        {
+          name: "Images",
+          extensions: ["png", "jpeg", "jpg", "tiff", "webp"],
+        },
+      ],
+    })) as string;
+
+    // Open and save the image to the global state.
+    try {
+      const picture: Picture = await invoke("get_image", {
+        path: file,
+      });
+      // If the image exists show it in the window.
+      displayImage(picture.width, picture.height, picture.data);
+    } catch (error) {
+      // If the image file could not be opened, display an error.
+      displayError(error as Error);
+    }
+  } catch (error) {
+    console.error(`Error: ${error}`);
   }
 }
 
-window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form")?.addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
-  });
+async function generate() {
+  try {
+    // Run the contamination algorithm on the input image.
+    const picture: Picture = await invoke("gen_image", {});
+    // Show the contaminated image in the window.
+    displayImage(picture.width, picture.height, picture.data);
+  } catch (error) {
+    console.error(`Error: ${error}`);
+  }
+}
+// Save the image as a png. The image size will match the
+// original input image.
+async function save() {
+  try {
+    const file = (await dialog.save({
+      defaultPath: "contaminated.png",
+      filters: [
+        {
+          name: "PNG",
+          extensions: ["png", "jpeg", "jpg"],
+        },
+      ],
+    })) as string;
+    await invoke("save_image", {
+      path: file,
+    });
+  } catch (error) {
+    console.error(`Error: ${error}`);
+  }
+}
+
+// Controls for the gui, two sliders a picker and 3 buttons.
+let controls = {
+  chooseImage: async function () {
+    chooseImage();
+  },
+  generate: async function () {
+    generate();
+  },
+  save: async function () {
+    save();
+  },
+};
+
+gui.add(controls, "chooseImage").name("Choose Image");
+gui.add(controls, "generate").name("Generate");
+gui.add(controls, "save").name("Save");
+
+// Convert the raw image data to a canvas image and put it on the canvas.
+function displayImage(width: number, height: number, data: Uint8Array) {
+  const splash = document.getElementById("splash");
+  splash!.style.display = "none";
+  const errorElement = document.getElementById("error-message");
+  if (errorElement instanceof HTMLElement) {
+    errorElement.textContent = "";
+    errorElement.style.display = "none";
+  }
+  const canvas = document.querySelector("canvas") as HTMLCanvasElement;
+  canvas.style.display = "block";
+  const aspect = width / height;
+  const ctx = canvas.getContext("2d");
+  canvas.height = W / aspect;
+  let clamped_data = new Uint8ClampedArray(data);
+  const img_data = new ImageData(clamped_data, width, height);
+  ctx!.putImageData(img_data, 0, 0);
+}
+
+function displayError(error: Error) {
+  const splash = document.getElementById("splash");
+  splash!.style.display = "none";
+  const canvas = document.querySelector("canvas") as HTMLCanvasElement;
+  canvas.style.display = "none";
+  const errorElement = document.getElementById("error-message");
+  if (errorElement instanceof HTMLElement) {
+    errorElement.textContent = error.toString();
+    errorElement.style.display = "block";
+  }
+}
+
+// Toggle the control panel.
+document.addEventListener("keydown", (event) => {
+  if (event.key === "c" || event.key === "C") {
+    gui.show(gui._hidden);
+  }
 });
